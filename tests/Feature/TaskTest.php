1ea6cb708f8057e4bf07dfaeab08833934d9478b
MariuszMalankiewicz <m2ario123@wp.pl>
2024-04-19 17:46:16 +0200

    New test_api_return_tasks_list
    model adding fillable for title, description and status. Migration
    update structure add string status default open. Test create task, next
    going to uri /api/tasks and check response 200 then database looking for this
    task

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,31 +2,47 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Task;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_api_return_tasks_list(): void
+    use RefreshDatabase;
+
+    public function test_api_return_empty_tasks_list(): void
     {
         $response = $this->getjson('/api/tasks');
 
         $response->assertStatus(200);
-
         $response->assertJsonStructure([
             'tasks' => [
                 '*' => [
                     'id',
                     'title',
                     'description',
+                    'status',
                     'created_at',
-                    'update_at'
+                    'update_at',
                 ]
             ]
         ]);
+    }
+
+    public function test_api_return_tasks_list(): void
+    {
+        $taskData = [
+            'title' => 'test title',
+            'description' => 'test description',
+            'status' => 'test status'
+        ];
+
+        Task::create($taskData);
+
+        $response = $this->getjson('/api/tasks');
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('tasks', $taskData);
     }
 }
