9f6af0d38864d8b5e693b497441af571230d96e7
MariuszMalankiewicz <m2ario123@wp.pl>
2024-05-24 13:53:57 +0200

    Initiation Repository pattern and add directary Task
    Initiation Repository pattern
    create Repositories directory in app/http
    
    Add directory Task
    add Task directory with task interface and class to repositories.

---
+++ app/Repositories/Task/TaskRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Repositories\Task;
+
+use App\Models\Task;
+
+class TaskRepository implements TaskRepositoryInterface
+{
+
+}
---
+++ app/Repositories/Task/TaskRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Repositories\Task;
+
+interface TaskRepositoryInterface
+{
+    
+}
