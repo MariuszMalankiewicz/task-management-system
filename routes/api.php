683ad9d452e6baf80a1317eb4434a330d18787ef
MariuszMalankiewicz <m2ario123@wp.pl>
2024-06-07 21:50:23 +0200

    Middleware sanctum for tasks
    Group auth route and add auth:sanctum middleware to apiResource route

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TaskController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function()
+{
+    Route::post('users', 'store');
+    Route::post('users/login', 'login');
+});
 
-
-Route::post('/users', [AuthController::class, 'store'])->name('users.store');
-Route::post('/users/login', [AuthController::class, 'login'])->name('users.login');
-
-Route::apiResource('tasks', TaskController::class)->except('create', 'edit');
+Route::apiResource('tasks', TaskController::class)->except('create', 'edit')->middleware('auth:sanctum');;
